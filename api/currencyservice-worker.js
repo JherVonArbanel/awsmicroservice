78317132bc55ad2b717f2dcf0fa8dda7866a2ef8
Jherzon <Jherzon@JHERZON-REM-LT>
2019-07-31 02:16:32 -0500

    Added lambda

--- api/currencyservice-worker.js
+++ api/currencyservice-worker.js
@@ -3,7 +3,8 @@ const httpsTools = require("./httpsTools")
 
 exports.handler = (event, context, callback) => {
   let countryCode = event['pathParameters']['code'];
-  var options = {
+
+  let options = {
   host: 'xecdapi.xe.com',
   port: 443,
   path: '/v1/convert_from.json/?from=USD&to=' + countryCode + '&amount=1',
@@ -13,27 +14,10 @@ exports.handler = (event, context, callback) => {
     }
   };
   
-  var httpsCall = new Promise((resolve, reject) => {
-    const req = https.request(options,
-      (res) => {
-        let body = '';
-        res.on('data', (chunk) => (body += chunk.toString()));
-        res.on('error', reject);
-        res.on('end', () => {
-          if (res.statusCode >= 200 && res.statusCode <= 299) {
-            resolve({statusCode: res.statusCode, headers: res.headers, body: JSON.parse(body)});
-          } else {
-            reject('Request failed. status: ' + res.statusCode + ', body: ' + body);
-          }
-        });
-      });
-    req.on('error', reject);
-    req.end();
-  });
-  
-  return httpsCall.then(result => {
-    var selectedItem = result.body.to
-                           .filter(item => item.quotecurrency == countryCode);
-    return httpsTools.Response200({rate:selectedItem[0].mid});
-  });
+  return httpsTools.SendHttps(options)
+                   .then(result => {
+                      let selectedItem = result.body.to
+                                               .filter(item => item.quotecurrency == countryCode);
+                      return httpsTools.Response200({rate:selectedItem[0].mid});
+                    });
 }
--- api/httpsTools.js
+++ api/httpsTools.js
@@ -9,5 +9,24 @@ module.exports = {
             },
             "body": JSON.stringify(body)
         };
+    },
+    SendHttps = (options) => {
+        return new Promise((resolve, reject) => {
+            const req = https.request(options,
+              (res) => {
+                let body = '';
+                res.on('data', (chunk) => (body += chunk.toString()));
+                res.on('error', reject);
+                res.on('end', () => {
+                  if (res.statusCode >= 200 && res.statusCode <= 299) {
+                    resolve({statusCode: res.statusCode, headers: res.headers, body: JSON.parse(body)});
+                  } else {
+                    reject('Request failed. status: ' + res.statusCode + ', body: ' + body);
+                  }
+                });
+              });
+            req.on('error', reject);
+            req.end();
+        });
     }
 }
